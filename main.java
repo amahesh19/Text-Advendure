50b0f97bbc25170da9c9247a5f96214a9e0736f8
Nicholas Bombardieri <nbombard@ualberta.ca>
2019-03-02 16:06:24 -0700

    oops

--- main.java
+++ main.java
@@ -3,7 +3,7 @@ import java.util.Map;
 
 public class main {	
 	public static void main(String args[]) {
-		Map<String, SceneTest> Scenes = new HashMap<String, SceneTest>();
+		Map<String, Scene> Scenes = new HashMap<String, Scene>();
 		Map<String, Boolean> CheckPoints = new HashMap<String, Boolean>();
 		String currentSceneName;
 		
