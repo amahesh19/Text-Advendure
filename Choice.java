0662c7449f598676c563bbd7518c9e9ee8a6e047
vyomea <43073294+vyomea@users.noreply.github.com>
2019-03-02 14:59:30 -0700

    Update Choice.java

--- Choice.java
+++ Choice.java
@@ -1,1 +1,68 @@
+import java.util.ArrayList;
 
+public class Choice{
+    private String optionText;
+    private String description;
+    private ArrayList flipCheckpoints;
+    private String scene;
+
+    public Choice(String oT, String d, ArrayList cR, ArrayList fC){
+        optionText = oT;
+        description = d;
+        checkpointsRequired = cR;
+        flipCheckpoints = fC;
+    }
+
+    public String getOptionText(){
+        return optionText;
+    }
+
+    public void setOptionText(String newOption){
+        optionText = newOption;
+    }
+
+    public String getDescription(){
+        return description;
+    }
+
+    public void setDescription(String newD){
+        description = newD;
+    }
+
+    public ArrayList getFlipCheckpoints(){
+        return flipCheckpoints;
+    }
+
+    public void setFlipCheckpoints(ArrayList newFC){
+        flipCheckpoints = newFC;
+    }
+
+    public String getScene(){
+        return scene;
+    }
+
+    public void setScene(String newScene){
+        scene = newScene;
+    }
+}
+
+public class CheckpointQuery{
+    private String checkpointName;
+    private Boolean wantedValue;
+
+    public String getCheckpointName(){
+        return checkpointName;
+    }
+
+    public void setCheckpointName(String newCheckpointName){
+        checkpointName = newCheckpointName;
+    }
+
+    public Boolean getWantedValue(){
+        return wantedValue;
+    }
+
+    public void setWantedValue(Boolean newWantedValue){
+        wantedValue = newWantedValue;
+    }
+}
