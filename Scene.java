3d55e1b663130a63547a4ab0bf15d9bc1d04ecd0
Nicholas Bombardieri <nbombard@ualberta.ca>
2019-03-02 13:09:32 -0700

    finished tentative scene class

--- Scene.java
+++ Scene.java
@@ -1,1 +1,29 @@
 
+
+
+public class Scene {
+
+    private String description;
+    private ArrayList choices;
+
+    public Scene(ArrayList description, ArrayList choices){
+
+    }
+
+    public ArrayList getChoices(){
+        return this.choices;
+    }
+
+    public void setChoices(ArrayList newChoices){
+        this.choices = newChoices;
+    }
+
+    public ArrayList getDescription(){
+        return this.description;
+    }
+
+    public void setDescription(ArrayList newDescription){
+        this.description = newDescription;
+    }
+
+}
